288ce65499ad7d48ea7dcc72d20d28a204190004
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-04-11 15:02:33 +0300

    made canges

---
+++ loops.js
@@ -0,0 +1,135 @@
+// Iterate 0 to 10 using for loop, do the same using while and do while loop
+
+for(let i=0;i<=10;i++){
+    console.log(i)
+
+    
+}
+
+
+
+
+
+
+// Iterate 10 to 0 using for loop, do the same using while and do while loop
+for(let i=10;i>=0;i++){
+    console.log(i)
+    break;
+}
+
+
+// Iterate 0 to n using for loop
+for(let i=1; i <= NaN; i++){
+    console.log(i)
+
+
+}
+
+
+// Write a loop that makes the following pattern using console.log():
+
+   
+    for(let i=0;i<=10;i++){
+        console.log(i**1)
+    
+    }
+
+
+
+//     Use loop to print the following pattern:
+
+// 0 x 0 = 0
+// 1 x 1 = 1
+// 2 x 2 = 4
+// 3 x 3 = 9
+// 4 x 4 = 16
+// 5 x 5 = 25
+// 6 x 6 = 36
+// 7 x 7 = 49
+// 8 x 8 = 64
+// 9 x 9 = 81
+// 10 x 10 = 100
+
+
+for(let i=0;i<=10;i++){
+    console.log(i*i)
+
+    
+}
+
+
+// Using loop print the following pattern
+
+//  i    i^2   i^3
+//  0    0     0
+//  1    1     1
+//  2    4     8
+//  3    9     27
+//  4    16    64
+//  5    25    125
+//  6    36    216
+//  7    49    343
+//  8    64    512
+//  9    81    729
+// 10    100   1000
+for(let i=0;i<=10;i++){
+    console.log(i^2)
+
+    
+}
+for(let i=0;i<=10;i++){
+    console.log(i^3)
+
+    
+}
+for(let i=0;i<=10;i++){
+    console.log(i*i)
+
+    
+}
+
+
+// Use for loop to iterate from 0 to 100 and print only even numbers
+
+for(let i=0;i<= 100; i++){
+console.log(i %2==0);
+
+}
+
+
+
+
+
+
+// Use for loop to iterate from 0 to 100 and print only odd numbers
+const IsNumber=[]
+for(let i = 0; i <= 100; i++){
+console.log(`${i} * ${i} = ${i * i}`)
+
+
+}
+
+
+
+// Use for loop to iterate from 0 to 100 and print only prime numbers
+for(let i=0;i<=100;i++){
+    console.log(i% i==0)
+console.log(i)
+    
+}
+
+
+// Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+for(let i=0;i< i.length; i++){
+    i=i+i
+    console.log(i)
+
+    
+}
+
+
+
+
+
+
+
